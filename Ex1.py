8a965fe23af3c6f079981ad6545fbab5cd8331b7
Santkey <70204224+Santkey@users.noreply.github.com>
2020-10-22 18:27:12 +0200

    Add files via upload

---
+++ Ex1.py
@@ -0,0 +1,15 @@
+print(5 + 7)
+print(5 - 2)
+print(5 * 6)
+print(5 / 5)
+print(10 % 3)
+
+print("Hello World!")
+print('World ' * 5)
+
+# Komentarz liniowy
+
+zmienna1 = "Hello"
+zmienna2 = input("Podaj imie: ")
+
+print(zmienna1 + " " + zmienna2)
